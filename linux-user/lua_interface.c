1e4243d52681b19b91ed5f25ec9a133e45633e6a
Ryan Hileman <lunixbochs@gmail.com>
2015-01-13 06:23:44 -0800

    basic open hook works

--- linux-user/lua_interface.c
+++ linux-user/lua_interface.c
@@ -1,32 +1,101 @@
-#include <stdio.h>
+#include <sys/mman.h>
 
 #include "lua.h"
 #include "qemu.h"
 
-abi_long lua_hook_syscall(
-    void *cpu_env, int num, abi_long arg1,
-    abi_long arg2, abi_long arg3, abi_long arg4,
-    abi_long arg5, abi_long arg6, abi_long arg7,
-    abi_long arg8) {
-    return do_syscall_nolua(cpu_env, num, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
-}
+typedef struct {
+    void *cpu_env;
+    int num;
+    abi_long args[8];
+} syscall_args;
 
+typedef struct {
+    abi_long addr, len;
+} syscall_map;
 
-/*
-int i;
-for (i = 0; i < nsyscalls; i++) {
-    if (scnames[i].nr == num) {
-        if (scnames[i].call != NULL) {
-            scnames[i].call(&scnames[i], arg1, arg2, arg3, arg4, arg5, arg6);
-        } else {
-            if (scnames[i].format != NULL)
-                format = scnames[i].format;
-            gemu_log(format, scnames[i].name, arg1, arg2, arg3, arg4, arg5, arg6);
-        }
-        return;
+static int orig_syscall = 0;
+static syscall_args orig_syscall_args = {0};
+static syscall_map syscall_munmap[8] = {{0}};
+
+static abi_long lua_arbitrary_param(lua_State *L, int n, abi_long def) {
+    int type = lua_type(L, n);
+    int len;
+    switch (type) {
+        case LUA_TNIL: return 0;
+        case LUA_TNUMBER: return (abi_long)lua_tonumber(L, n);
+        case LUA_TSTRING:
+            len = lua_rawlen(L, n);
+            abi_long tmp = target_mmap(0, len, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
+            memcpy((void *)g2h(tmp), lua_tostring(L, n), len);
+            syscall_munmap[n - 1].addr = tmp;
+            syscall_munmap[n - 1].len = len;
+            return tmp;
+        case LUA_TNONE:
+            return def;
+        default:
+            fprintf(stderr, "unsupported lua type: %s\n", lua_typename(L, type));
+            return def;
     }
 }
-*/
+
+static int lua_syscall_orig(lua_State *L) {
+    if (orig_syscall != 0) {
+        syscall_args *sargs = &orig_syscall_args;
+        abi_long args[8];
+        int i, ret;
+        for (i = 0; i < 8; i++) {
+            args[i] = lua_arbitrary_param(L, i + 1, sargs->args[i]);
+        }
+        ret = do_syscall_nolua(sargs->cpu_env, sargs->num,
+                               args[0], args[1], args[2], args[3],
+                               args[4], args[5], args[6], args[7]);
+        lua_pushnumber(L, ret);
+        for (i = 0; i < 8; i++) {
+            syscall_map *m = &syscall_munmap[i];
+            if (m->addr) {
+                target_munmap(m->addr, m->len);
+                m->addr = 0;
+                m->len = 0;
+            }
+        }
+        return 1;
+    }
+    return 0;
+}
+
+abi_long lua_hook_syscall(
+        void *cpu_env, int num, abi_long a1,
+        abi_long a2, abi_long a3, abi_long a4,
+        abi_long a5, abi_long a6, abi_long a7,
+        abi_long a8) {
+    int ret;
+    orig_syscall = 1;
+    syscall_args *a = &orig_syscall_args;
+    a->cpu_env = cpu_env;
+    a->num = num;
+    a->args[0] = a1, a->args[1] = a2, a->args[2] = a3, a->args[3] = a4;
+    a->args[4] = a5, a->args[5] = a6, a->args[6] = a7, a->args[7] = a8;
+    lua_State *L = lua_state;
+    switch (num) {
+        case TARGET_NR_open:
+            lua_getglobal(L, "open");
+            if (!lua_isfunction(L, -1)) {
+                lua_pop(L, -1);
+            } else {
+                lua_pushstring(L, g2h(a1));
+                lua_pushnumber(L, a2);
+                lua_pushnumber(L, a3);
+                lua_call(L, 3, 1);
+                int ret = lua_tonumber(L, -1);
+                lua_pop(L, -1);
+                return ret;
+            }
+            break;
+    }
+    ret = do_syscall_nolua(cpu_env, num, a1, a2, a3, a4, a5, a6, a7, a8);
+    orig_syscall = 0;
+    return ret;
+}
 
 static int lua_err_tostring(lua_State *L) {
     double d = lua_tonumber(L, 1);
@@ -53,5 +122,6 @@ void lua_set_globals(lua_State *L) {
     lua_pushcfunction(L, lua_err_tostring);
     lua_settable(L, -3);
     lua_setglobal(L, "err");
-    printf("lua_set_globals finished\n");
+
+    lua_register(L, "orig", lua_syscall_orig);
 }
